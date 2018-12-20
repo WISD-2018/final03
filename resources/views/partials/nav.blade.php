536a6289fd171a45bd937db8cf9ca94009bf9f68
3A532088 <paileo007007@gmail.com>
2018-12-20 20:37:31 +0800

    首頁建置

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{ action('HomeController@index') }}">書籍線上訂購</a>
+        <ul class="nav navbar-nav">
+            <li>
+                <a href="">書籍查詢</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div style="padding-top: 70px;"></div>
