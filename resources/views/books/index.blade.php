24e53ae2a0f5712f01e70f5d9de1fdbbed7b631e
3A532088 <paileo007007@gmail.com>
2019-01-15 23:17:18 +0800

    增加route及回傳

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -7,13 +7,13 @@
     <form action="/cart" method="post" accept-charset="UTF-8">
         {{ csrf_field() }}
     <div style="display: inline-block;" >
-        <img class="ban" src="" style="height: 320px;"></div>
+        <img class="ban" src="{{ $book->imgurl}}" style="height: 320px;"></div>
     <div style="display: inline-block;">
-        <h4><b></b></h4>
-        <h4>作者:</h4>
-        <h4>出版社:</h4>
-        <h4>出版日期:</h4>
-        <h4 style="color:red"><b>定價:元</b></h4>
+        <h4><b>{{ $book->book_name}}</b></h4>
+        <h4>作者:{{$book->author}}</h4>
+        <h4>出版社:{{$book->publisher}}</h4>
+        <h4>出版日期:{{$book->pub_date}}</h4>
+        <h4 style="color:red"><b>定價:{{$book->book_price}}元</b></h4>
         <button type="submit" class="btn btn-default">
             <i class="fa fa-plus"></i>加入購物車
         </button>
@@ -27,9 +27,13 @@
         簡介:
     </div>
     <div>
-
+        {{$book->summary}}
     </div>
 
-       
+        <input type="hidden" name="book_id" value="{{ $book->id}}">
+        <input type="hidden" name="book_name" value="{{ $book->book_name}}">
+        <input type="hidden" name="book_price" value="{{ $book->book_price}}">
+        <input type="hidden" name="imgurl" value="{{ $book->imgurl}}">
+        <input type="hidden" name="count" value="1">
     </form>
 @endsection
