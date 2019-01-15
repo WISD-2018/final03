721a1ae50c809987b99908d295f1e27c29c631f8
3A532088 <paileo007007@gmail.com>
2019-01-15 22:42:11 +0800

    BookController show

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,6 +13,12 @@ class BookController extends Controller
         ]);
     }
 
+    public function show($id){
+        $book=Book::find($id);
+        $data=['book'=>$book];
+        return view('books.index', $data);
+    }
+
     public function search(Request $request){
         $tasks=Book::orderBy('id')->where('book_name','like',"%".$request->booksearch."%")->orwhere("author",'like',"%".$request->booksearch."%")->get();
         return view('books.search', [
