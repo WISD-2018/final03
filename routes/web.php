8646163b303e46b35a5cac60f73cb42cf2e8db64
3A532088 <paileo007007@gmail.com>
2019-01-15 22:18:40 +0800

    新增各頁面route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,18 @@
 
 Route::get('/', 'HomeController@index') ;
 Route::get('/booksearch', 'BookController@index') ;
+Route::get('/booksearchclick', 'BookController@search') ;
 Route::get('/ordersearch', 'OrderController@index') ;
+Route::get('/carts', 'CartController@index') ;
+
+Route::get('/book/{id}/show', ['as' => 'books.show', 'uses' => 'BookController@show']) ;
+Route::get('/checkout', 'OrderController@checkout') ;
+
+Route::post('/order', 'OrderController@store');
+Route::post('/cart', 'CartController@store');
+Route::delete('carts/{id}',['as'=>'carts.destroy','uses'=>'CartController@destroy']);
+Route::get('carts/{id}/edit',['as'=>'carts.edit','uses'=>'CartController@edit']);
+Route::patch('carts/{id}',['as'=>'carts.update','uses'=>'CartController@update']);
 
 Route::auth();
 Route::get('/home', function () {
