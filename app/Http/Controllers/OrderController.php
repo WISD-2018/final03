5c09e5d78771aa6fd71fcabe8b725955729e8ef3
3A532090 <raxttc@gmail.com>
2019-01-15 22:38:12 +0800

    修改OrderControll@index

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart;
+use App\Order;
+use App\Repositories\TaskRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    public function index(){
-        return view('orders.search');
+    protected $carts;
+
+    public function __construct(TaskRepository $carts)
+    {
+        $this->middleware('auth');
+
+        $this->carts = $carts;
     }
+
+    public function index(Request $request)
+    {
+        $orders = Order::where('user_id', $request->user()->id)->get();
+
+        return view('orders.search', [
+            'orders' => $orders,
+        ]);
+    }
+
 }
