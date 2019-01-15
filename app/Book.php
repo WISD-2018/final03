eeda117da9c0342eaf871b6e3e7b009945a54159
3A532088 <paileo007007@gmail.com>
2019-01-15 22:49:53 +0800

    編輯Book Model

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $table = "books";
+    protected $fillable = ['book_name','book_price','author','publisher','pub_date','summary'];
+
+    public function carts()
+    {
+        return $this->belongsTo(Cart::class);
+    }
 }
